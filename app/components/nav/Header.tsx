73bd4739785dea535afdced090ccfd40eb1709c7
NEO <neo.one.mail@gmail.com>
2023-03-24 23:46:46 +0200

    add toggle theme button

--- app/components/nav/Header.tsx
+++ app/components/nav/Header.tsx
@@ -1,22 +1,33 @@
+"use client"
+import useThemeMode from "@/app/hooks/useThemeMode"
 import Image from "next/image"
 import Link from "next/link"
 import React from "react"
+import ThemeToggle from "./ThemeToggle"
 
-const Header: React.FC = () => <header className="bg-gray-900">
+const Header: React.FC = () => {
+    const [colorMode, setColorMode] = useThemeMode()
+    const changeTheme = () => setColorMode(colorMode === "light" ? "dark" : "light")
 
-    <nav className="flex flex-col px-1 py-1 sm:flex-row sm:justify-between sm:items-center">
 
-        <Link href="/">
-            <Image
-                src="/logo.png"
-                width={40}
-                height={40}
-                className="rounded-xl w-auto h-6 sm:h-7"
-                alt={"logo"}
-            />
-        </Link>
-        <div className="flex items-center mt-2 -mx-2 sm:mt-0" />
-    </nav>
-</header>
+    return (
+        <header className=" dark:bg-gray-900">
+            <nav className="flex flex-col px-1 py-1 sm:flex-row sm:justify-between sm:items-center">
+                <Link href="/">
+                    <Image
+                        src="/logo.png"
+                        width={40}
+                        height={40}
+                        className="rounded-xl w-auto h-6 sm:h-7"
+                        alt={"logo"}
+                    />
+                </Link>
+
+
+                <ThemeToggle />
+            </nav>
+        </header>
+    )
+}
 
 export default Header
